73803b13ec8f284ebe6660a46dc427d15e664341
RIALI MOUAD <hamza.sbaai@um6p.ma>
2022-02-05 19:19:01 +0100

    commit

---
+++ Agenda/src/main/java/com/example/Agenda_C/domain/Role.java
@@ -0,0 +1,24 @@
+package com.example.Agenda_C.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "role")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role {
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String label;
+
+
+}
